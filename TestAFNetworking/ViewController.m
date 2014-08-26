e391877b2679895b119476256bd8fec5d745ae94
Keita Ito <gi1okt@gmail.com>
2014-08-25 23:57:26 -0700

    added AFNetworking, added AFHTTPSessionManager method

--- TestAFNetworking/ViewController.m
+++ TestAFNetworking/ViewController.m
@@ -19,12 +19,30 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"TestAFNetworking");
+    [self getURL:@"http://graph.facebook.com/keitaitok"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)getURL:(NSString *)string
+{
+    // Initialize
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    // Get JSON file with using AFHTTPSessionManager
+    [manager GET:string
+      parameters:nil
+         success:^(NSURLSessionDataTask *task, id responseObject) {
+             // Success
+             NSLog(@"responseObject: %@", responseObject);
+         } failure:^(NSURLSessionDataTask *task, NSError *error) {
+             // Error
+             NSLog(@"Error: %@", error);
+         }];
 }
 
 @end
